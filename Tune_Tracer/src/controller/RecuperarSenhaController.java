1c4acaacbd12cde6a0262a36a58efc9d8c35a064
anasotero <2022327225@ifam.edu.br>
2023-10-04 06:54:00 -0400

    Recuperar senha e Sons

---
+++ Tune_Tracer/src/controller/RecuperarSenhaController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.sql.SQLException;
+import model.bo.RecuperarSenhaBO;
+import model.dao.ConexaoSQL;
+import model.vo.RecuperarSenhaVO;
+import java.sql.Connection; // Importe a classe Connection aqui
+
+public class RecuperarSenhaController {
+    private RecuperarSenhaBO recuperarSenhaBO;
+    private Connection conexao; // Adicione uma referência à conexão aqui
+
+    public RecuperarSenhaController(Connection conexao) {
+        this.conexao = conexao;
+        recuperarSenhaBO = new RecuperarSenhaBO(conexao); // Passe a conexão para o construtor de RecuperarSenhaBO
+    }
+
+	public boolean processarRecuperacaoSenha(String email, String palavraChave, String novaSenha) {
+        RecuperarSenhaVO recuperarSenhaVO = new RecuperarSenhaVO(email, palavraChave, novaSenha);
+
+        try {
+            return recuperarSenhaBO.alterarSenha(recuperarSenhaVO);
+        } catch (SQLException e) {
+            System.err.println("Erro");
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
